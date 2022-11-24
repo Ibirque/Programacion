59bfcd55a01cccee51e6f0302d185db9f43dd279
Alumno <Alumno@PC15690>
2022-11-24 08:56:39 +0100

    ejemplos de clase

---
+++ src/main/java/proyectos/ibirque/Uf2_E000.java
@@ -0,0 +1,33 @@
+
+package proyectos.ibirque;
+
+
+public class Uf2_E000 {
+    int artf= 0;
+
+    public static void main(String[] args) {
+       //aqui hay que usar el nombre de la clase que se esta creando
+       OrdenarDescendente programa = new OrdenarDescendente();
+       programa.inicio();
+    
+    }
+
+    public static int funcionInt(){
+    
+        return 0;
+    }
+    public static float funcionFloat(){
+    
+        return 0f;
+    }
+    public static boolean funcionBool(){
+    
+        return false;
+    }
+
+    private static class OrdenarDescendente {
+        public OrdenarDescendente() {}
+        private void inicio() {throw new UnsupportedOperationException("Not supported yet."); }
+    }
+    
+}
---
+++ src/main/java/proyectos/ibirque/Uf2_E001VariableLocal.java
@@ -0,0 +1,46 @@
+
+package proyectos.ibirque;
+
+import java.util.Scanner;
+
+
+public class Uf2_E001VariableLocal {
+
+    //variabl global
+    private int[] lista = new int[10];
+    
+    public static void main(String[] args) {
+        
+        //Scanner entrada = new Scanner(System.in);
+        //creamos una instancia de esta clase
+        Uf2_E001VariableLocal programa = new Uf2_E001VariableLocal();
+        programa.inicio();      
+    }
+    
+    public void inicio(){
+        //bloque de codigo
+        //instrucciones del metodo inicio
+        //problema general
+        
+        leerLista();
+        ordenarLista();
+        imprimirLista();
+    }
+    
+    //metodo para leer una lista
+    //lo podemos llamar desde inicio que es nuestro nuevo metodo principal
+    public void leerLista(){
+        //bloque
+    }
+    
+    //metodo para ordenar la lista
+    public void ordenarLista(){
+        //bloque
+    }
+    
+    //metodo para imprimir la lista
+    public void imprimirLista(){
+        //bloque
+    }
+    
+}
