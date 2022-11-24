92900014619c9cac8fb421fbd9ee44b33f6149ef
javie <javie@Javier.home>
2022-11-24 08:56:56 +0100

    UF2

---
+++ src/main/java/UF2/E001variableGlobal.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package UF2;
+
+
+/**
+ *
+ * @author javie
+ */
+public class E001variableGlobal {
+
+ // delcaracion de variable globa
+    private int [] lista = new int[10];
+    public static void main(String[] args) {
+        E001variableGlobal programa = new E001variableGlobal();
+        programa.inicio();
+    }
+    public void inicio(){
+        //bloque de codigo
+        //instrucciones del metodo de incio
+        //problema general
+        leerLista();
+        ordenarLista();
+        imprimirLista();
+    }
+    //Metodo para leer datos de la lista 
+    public void leerLista(){
+        
+    }
+    public void ordenarLista(){
+        
+    }
+    public void imprimirLista(){
+        
+    }
+}
