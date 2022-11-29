c32208faaa3e76fc0616415d209389393dd848c0
Alumno <Alumno@PC15690>
2022-11-29 09:23:54 +0100

    clase

---
+++ src/main/java/proyectos/ibirque/Observatorio.java
@@ -0,0 +1,63 @@
+
+package proyectos.ibirque;
+
+
+public class Observatorio {
+
+    //float array[7][52]
+    
+    public static void main(String[] args) {
+        
+        
+    
+    }
+
+    //comprobar el dia de la semana
+        //lun=1, mar=2 ... dom = 7
+        //comprobar el null
+        //esta clase retorna int, y depende del numero es lunes ... domingo
+    
+    
+    //clase registrar
+    
+    //clase consultar media
+        //bucle for comparar las temperaturas mayres o menores
+    
+    //clase cosultar la diferencia maxima
+    
+    //clase de cierre
+
+    //menu
+        //imprimir texto
+        //sucesion de system.out.println
+        //no tiene que descomponerse mas
+    
+    //subproblema 2 - tratar el roden
+        //leer orden por teclado
+        //entrar registro de temp
+        //mostrar temp media
+        //mostrar dif maxima
+        //finalizar la ejecucion del programa
+    
+    //tercer nivel de descomposicion
+        //subproblema > entrar registro de temp semanalas
+        //leer temp y ponerlas en el array
+        //actualizar la fecha > NO es simple, hay que controlar que dia acaba cada mes (28,30,31)
+
+    
+    //mostrar menu
+    /*tratar el orden
+        a. entrar registro de temp
+          i.leer temp por tecl
+          ii.actualizar fecha
+        b. mostrar temp media
+          i.mostrar fecha
+          ii.calcular temp media
+        c. mostrar dif maxima
+          i.mostrar fecha act
+          ii.calcular dif maxima
+        d. finalizar la ejecucion del programa */                        
+    
+    
+    
+}
