a0b436ef35bc740eeba3e987bb50db28b59963c2
Alumno <Alumno@PC15690>
2023-01-12 08:25:35 +0100

    reto completado

--- src/main/java/proyectos/ibirque/Reto.java
+++ src/main/java/proyectos/ibirque/Reto.java
@@ -13,7 +13,11 @@ public static void main(String[] args) {
     public void inicio() {
         int [] valores = {6, 5, 3, 10, 4};
         double resultado = media(valores);
-        System.out.println("La media aritmetica es " + resultado);
+        
+        String notaFinal = notaT(resultado);
+        
+        //System.out.println("La media aritmetica es " + resultado);
+        System.out.println("Con un promedio de  " +resultado+" tu nota es un "+notaFinal);
     }
 
     public double media(int[] lista) {
@@ -29,6 +33,24 @@ public static void main(String[] args) {
         
         return sumatorio / lista.length;
     }
+    
+        
 
+     public String notaT(double nota){
+        //nota tiene el valor que hay que tratar
+        String texto = null;
+        if((nota >=9) && (nota <=10)){
+            texto = "Excelente";
+        } else if((nota >=6.5) && (nota <9)) {
+            texto = "Notable";
+        }else if((nota >=5) && (nota <6.5)) {
+            texto = "Suficiente";
+        }else if((nota >=0) && (nota <5)) {
+            texto = "Suspendido";   
+        } else {
+            texto = "Nota no vÃ¡lida";
+        }
+        return texto;
+    }
     
 }
