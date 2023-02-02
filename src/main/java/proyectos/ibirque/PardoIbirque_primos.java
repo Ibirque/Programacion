97502fda871ad2fa6b71cd8fe1ee3e2d5dca677e
Alumno <Alumno@PC15690>
2023-02-02 09:54:43 +0100

    codigo mejroado

--- src/main/java/proyectos/ibirque/PardoIbirque_primos.java
+++ src/main/java/proyectos/ibirque/PardoIbirque_primos.java
@@ -1,12 +1,27 @@
-
 package proyectos.ibirque;
 
 public class PardoIbirque_primos {
 
-    /**/
-    
+    /*QUE QUEREMOS HACER?*/
+ /*Queremos un programa que sea capaz de encontrar numeros primos
+    -Los numeros primos solo se pueden dividir entre 1 y ellos mismos
+    -Queremos una clase principal que lea por teclado un numero
+        
+    -Descomponemos el numero y mostramos por pantalla los primos que lo forman
+    -Si el numero es primo, debemos mostrarlo por pantalla
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PardoIbirque_primos progr = new PardoIbirque_primos();
+        progr.inicio();
+    }
+
+    public void inicio() {
+        captura();
+    }
+
+    
+    public void captura(){
+        //Recogemos el input del usuario        
     }
     
 }
