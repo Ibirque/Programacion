f203f7e9f3028e2b2b715d77b2c1a7ecfab27e2b
Ibir <Ibir@192.168.0.173>
2023-02-02 16:08:41 +0100

    numeros primos

--- src/main/java/proyectos/ibirque/PardoIbirque_primos.java
+++ src/main/java/proyectos/ibirque/PardoIbirque_primos.java
@@ -1,5 +1,7 @@
 package proyectos.ibirque;
 
+import java.util.Scanner;
+
 public class PardoIbirque_primos {
 
     /*QUE QUEREMOS HACER?*/
@@ -16,12 +18,73 @@ public class PardoIbirque_primos {
     }
 
     public void inicio() {
-        captura();
+        int x;
+        x = captura();
+        comprobar(x);
     }
 
-    
-    public void captura(){
-        //Recogemos el input del usuario        
+    public int captura() {
+
+        int x = 0;
+        //Boolean para atrapar al usuario
+        boolean bandera = false;
+        //Recogemos el input del usuario  
+        Scanner en = new Scanner(System.in);
+        System.out.println("Introduce un numero positivo, mayor a 2");
+
+        do {
+            if (en.hasNextInt()) {
+                x = en.nextInt();
+                if (x > 2) {
+                    bandera = true;
+                } else {
+                    System.out.println("Mayor a 2 y positivo");
+                }
+            } else {
+                System.out.println("Introduce un numero positivo mayor a 2, no otra cosa");
+                en.nextLine();
+            }
+        } while (!bandera);
+
+//Devolvemos el numero
+        return x;
     }
-    
+
+    public boolean comprobar(int x) {
+        //Nuestro iterador, lo inicializamos a 2 porque se
+        //sobreentiende que todos los numeros son divisibles por "1"
+        //Usaremos este iterador para dividir los numeros
+        int i = 2;
+        boolean bandera = false;
+        
+        //Iniciamos a true el boolean, y en caso de detectar lo contrario
+        //mas adelante, nos cargamos la bandera
+        boolean primo = true;
+        
+        //Creamos un bucle que no va a parar hasta que "i" tenga el mismo
+        //tamaño que "x"
+        do {
+            //En caso de que el bucle pueda dividir y obtener un modulo "0"
+            if (x % i == 0) {
+                
+                //Este if muestra por pantalla una sola vez el mensaje
+                if(!bandera){
+                    System.out.println("Los factores primos son: ");
+                    bandera = true;
+                    primo = false;
+                }
+                System.out.print(i+" ");
+                
+            }
+            i++;
+            //System.out.println("Prueba numero " + (i - 1) + " - " + (x % i));
+            //Probamos 19x7 = 133 para ver las iteraciones
+        } while (i != x);
+
+        if(primo){
+            System.out.println(x+" Es primo!");
+        }
+        return false;
+    }
+
 }
