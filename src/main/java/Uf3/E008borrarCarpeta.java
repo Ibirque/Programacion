46b3396c9ee0cb59ade2346134f6eb28fbf01fce
Alumno <Alumno@PC15684>
2023-02-17 10:49:47 +0100

    Ejercicio 8 corregido

--- src/main/java/Uf3/E008borrarCarpeta.java
+++ src/main/java/Uf3/E008borrarCarpeta.java
@@ -12,46 +12,53 @@ public class E008borrarCarpeta {
 
     //Queremos:
     /*Crea un programa que lea desde el teclado el texto asociado a la 
-        ruta de una carpeta existente en el ordenador. 
-        Borrar todos los elementos que tenga
-        Para borrar una carpeta, antes hay que vaciarla
-        Solución recursiva?*/
+      ruta de una carpeta existente en el ordenador. 
+      Borrar todos los elementos que tenga
+      Para borrar una carpeta, antes hay que vaciarla
+      Solución recursiva?*/
     public void inicio() {
+        /*MI METODO*/
         //Ruta es --> C:/temp
         String ruta = Devolver();
-        
         listar(ruta);
+
+        /*METODO CORREGIDO POR MARIA*/
+        File Nuevo = leerRutaCarpeta();
+        borrar(Nuevo);
+
     }
 
     public void listar(String A) {
         File contenido = new File(A);
-        File[] verCont = contenido.listFiles(); 
-        
+        File[] verCont = contenido.listFiles();
+
         //Usamos un bucle para recorrer todos los elementos de nuestra carpeta
-        System.out.println("El contenido de "+contenido.getAbsolutePath() + " es: ");
-        for(int i =0; i<verCont.length; i++){
+        System.out.println("El contenido de " + contenido.getAbsolutePath() + " es: ");
+        for (int i = 0; i < verCont.length; i++) {
             File f = verCont[i];
-            if(f.isDirectory()){System.out.println("[DIR]");}
-            else{System.out.println("[FILE]");}
+            if (f.isDirectory()) {
+                System.out.println("[DIR]");
+            } else {
+                System.out.println("[FILE]");
+            }
             System.out.println(f.getName());
         }
-        
+
         System.out.println("¿Estas seguro de que quieres destruir todo el contenido de esta carpeta? [Y/N]");
         Scanner ent = new Scanner(System.in);
         String respuesta = ent.nextLine();
         respuesta = respuesta.toLowerCase();
-        
-        
+
         boolean glag = false;
-        do{
-        if(respuesta.charAt(0) == 'y'){
-            Destruir(A);
-            glag = true;
-        }else{
-            System.out.println("Cerrando el programa");
-            glag = true;
-        }
-        }while(!glag);
+        do {
+            if (respuesta.charAt(0) == 'y') {
+                Destruir(A);
+                glag = true;
+            } else {
+                System.out.println("Cerrando el programa");
+                glag = true;
+            }
+        } while (!glag);
     }
 
     public void Destruir(String A) {
@@ -81,13 +88,54 @@ public class E008borrarCarpeta {
         Scanner ent = new Scanner(System.in);
         System.out.println("Introduce la ruta: ");
         hold = ent.nextLine();
-        
-        
+
         return hold;
-        
+
         //comprobar si es file o directory
-        
-        
+    }
+
+    
+    /*METODO CORREGIDO POR MARIA */
+     //preguntar al usuario el nombre de la carpeta
+    //param de entrada => nope
+    //param de salida => la ruta asociada al File
+    public File leerRutaCarpeta() {
+        File f = null;
+        //inicializar a null un tipo File (tipo compuesto)
+        boolean preguntar = true;
+        Scanner entrada = new Scanner(System.in);
+        while (preguntar) {
+            System.out.println("Escribe la ruta a la carpeta");
+            String nombreCarpeta = entrada.nextLine();
+            f = new File(nombreCarpeta);
+            if (f.isDirectory()) {
+                preguntar = false;
+            } else {
+                System.out.println("Esa carpeta no existe");
+            }
+        }
+        return f;
+    }
+
+    //borrar el contenido
+    //param entrada => lo que devuelve la funciÃ³n anterior, la ruta
+    //param salida => nope
+    public void borrar(File ruta) {
+        File[] elementos = ruta.listFiles();
+        //recorrer el array
+        for(int i = 0; i < elementos.length; i++){
+            if(elementos[i].isFile()){
+                //si valida true, lo borramos
+                elementos[i].delete();
+            }
+            else if(elementos[i].isDirectory()){
+                //si valida true, hay que vaciarla primero
+                //llamar a esta misma funciÃ³n
+                borrar(elementos[i]);
+                //cuando estÃ¡ vacÃ­a, se puede borrar
+                elementos[i].delete();
+            }
+        }
     }
 
 }
