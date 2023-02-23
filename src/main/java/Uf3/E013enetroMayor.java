00cdec2db479d256ab48a9106aeca2c8d86a5bde
Alumno <Alumno@PC15690>
2023-02-23 09:11:34 +0100

    E013 subido

--- src/main/java/Uf3/E013enetroMayor.java
+++ src/main/java/Uf3/E013enetroMayor.java
@@ -11,20 +11,49 @@ public class E013enetroMayor {
     }
 
     public void inicio() {
+        EstiloMaria();
+    }
+
+    public void AMiManera() {
         int max = 0;
         try {
             File f = new File("C:/Temp/ARGH.txt");
             Scanner en = new Scanner(f);
             for (int i = 0; i < 10; i++) {
                 int valor = en.nextInt();
-                if(valor>max){
+                if (valor > max) {
                     max = valor;
                 }
             }
+            en.close();
         } catch (Exception e) {
             System.out.println("Hemos encontrado un problema: " + e);
         }
-        System.out.println("El valor maximo del archivo es: "+max);
+        System.out.println("El valor maximo del archivo es: " + max);
+        
+    }
+
+    public void EstiloMaria() {
+        Scanner lectura = null;
+        try {
+            File f = new File("C:/Temp/ARGH.txt");
+            lectura = new Scanner(f);
+            //premica
+            int max = lectura.nextInt();
+            //recorrido
+            for (int i = 0; i < 10; i++) {
+                int valor = lectura.nextInt();
+                if (max < valor) {
+                    max = valor;
+                }
+            }
+            System.out.println("El mayor es: " + max);
+        } catch (Exception e) {
+            System.out.println("Hay un error en: " + e);
+        }
+        
+        lectura.close();
+        
     }
 
 }
