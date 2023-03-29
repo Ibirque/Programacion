79fefdf5d7cc3722c7fbbb29591af72643c6b461
Alumno <Alumno@PC15690>
2023-03-29 09:57:53 +0200

    ksksksks

--- src/main/java/Uf4/DiscosMain.java
+++ src/main/java/Uf4/DiscosMain.java
@@ -12,7 +12,6 @@ public class DiscosMain {
     4-MODIFICAR
     5-SALIR
      */
-    int NUMDISCOS = 0;    
     ArrayList<Object> nombreDiscos = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -27,9 +26,7 @@ public class DiscosMain {
         boolean flag = false;
         Scanner ent = new Scanner(System.in);
         int entrada = 0;
-        
-        
-        
+
         do {
             if (ent.hasNextInt()) {
                 entrada = ent.nextInt();
@@ -71,37 +68,16 @@ public class DiscosMain {
 
     public void Listar() {
         //primero queremos pedir al usuario que es lo que quiere listar        
-        Scanner ent = new Scanner(System.in);
-        int entrada;
-        boolean flag = false;       
-
-        System.out.println("Introduce un numero que ya exista en nuestra base de datos, de momento hay: " + NUMDISCOS + " discos registrados.");
-        do {
-            if (ent.hasNextInt()) {
-                //tenemos el int, comprobamos si nos pasamos del numero total de albumes que tenemos en neustro programa
-                entrada = ent.nextInt();
-                if (entrada > NUMDISCOS) {
-                    //El usuario se ha pasado de listo, esta pidiendo algo que no existe, pedir de nuevo el input
-                    System.out.println("Ese ID no esta registrado");
-                    ent.nextLine();
-                } else {
-                    //Ahora llamamos al numero de disco                     
-                    System.out.println("aa"+nombreDiscos.get((entrada-1)));
-                    break;
-                }
-            } else {
-                //No ha introducido un numero valido
-                System.out.println("Por favor introduce un numero valido");
-                ent.nextLine();
-            }
-        } while (!flag);
+        for (int i = 0; i < nombreDiscos.size(); i++) {
+            System.out.println("" + nombreDiscos.get(i));
+        }
     }
 
     public void Anyadir() {
         //En la funcion añadir queremos crear un nuevo elemento desde cero, es decir generar un nuevo objeto
         //Para poder conseguir esto vamos a crear un array de tamaño X, mas adelante averiguaremos si podemos aumentar el tamaño del array
 
-        NUMDISCOS = NUMDISCOS + 1;
+        int buffer;
 
         Scanner ent = new Scanner(System.in);
         //Nuestro constructor es el siguiente: Discos(String autor, String titulo, String genero, String id, int duracion)
@@ -113,7 +89,10 @@ public class DiscosMain {
             String titulo = "";
             String genero = "";
             int duracion = 0;
-            Discos x = new Discos(autor, titulo, genero, NUMDISCOS, duracion);
+            Discos x = new Discos(autor, titulo, genero, 0, duracion);
+            buffer = (nombreDiscos.size() + 1);
+
+            x.setId(buffer);
 
             System.out.println("\nIntroduce el autor");
             autor = ent.nextLine();
@@ -161,7 +140,7 @@ public class DiscosMain {
                     ent.nextLine();
                 } else {
                     //Ahora llamamos al numero de disco                              
-                    totalDiscos[entrada] = null;
+                    nombreDiscos.remove((entrada - 1));
                     System.out.println("El elemento numero " + entrada + " ha sido eliminado de la base de datos");
                     flag = true;
                 }
@@ -174,6 +153,8 @@ public class DiscosMain {
     }
 
     public void Modificar() {
+        //listamos los elementos para que el usuario pueda elegir
+        Listar();
         //Ahora queremos modificar, le pregutnamos el numero y le listamos el elemento primero para que lo vea
 
         Scanner ent = new Scanner(System.in);
@@ -182,77 +163,51 @@ public class DiscosMain {
         //Listamos  
         int entrada = 0;
         boolean flag = false;
-        String xd;
+        int buffer;
+        int duracion = 0;
 
-        System.out.println("Introduce un numero que ya exista en nuestra base de datos, de momento hay: " + NUMDISCOS + " discos registrados.");
+        System.out.println("Introduce un numero que ya exista en nuestra base de datos, de momento hay: " + nombreDiscos.size() + " discos registrados.");
+
         do {
             if (ent.hasNextInt()) {
-                //tenemos el int, comprobamos si nos pasamos del numero total de albumes que tenemos en neustro programa
                 entrada = ent.nextInt();
-                if (entrada > NUMDISCOS) {
-                    //El usuario se ha pasado de listo, esta pidiendo algo que no existe, pedir de nuevo el input
-                    System.out.println("Ese ID no esta registrado");
+                if (entrada<(nombreDiscos.size()+1)) {
+                 
+                    
+                    Discos x = new Discos("", "", "", 0, 0);
+                    buffer = (nombreDiscos.size());
+
+                    x.setId(entrada);
+
+                    System.out.println("\nIntroduce el autor");
+                    x.setAutor(ent.nextLine());
+
+                    System.out.println("\nIntroduce el titulo");                   
+                    x.setTitulo(ent.nextLine());
+
+                    System.out.println("\nIntroduce el genero");
+                    x.setGenero(ent.nextLine());
+
+                    System.out.println("\nIntroduce la duracion del album en minutos");
+                    if (ent.hasNextInt()) {
+                        duracion = ent.nextInt();
+                        x.setDuracion(duracion);
+                        System.out.println("Datos introducidos correctamente!");
+                        flag = true;
+                    } else {
+                        System.out.println("Por favor introduce un numero");
+                    }
+                    nombreDiscos.set(entrada, x);
+                }else{
+                    System.out.println("Por favor introduce un numero valido");
                     ent.nextLine();
-                } else {
-                    //Ahora llamamos al numero de disco                              
-                    xd = totalDiscos[entrada].toString();
-                    System.out.println(xd);
-                    break;
                 }
+                
             } else {
-                //No ha introducido un numero valido
-                System.out.println("Por favor introduce un numero valido");
+                System.out.println("Por favor introduce un numero correcto");
                 ent.nextLine();
             }
         } while (!flag);
-
-        //Nuestro constructor es el siguiente: Discos(String autor, String titulo, String genero, String id, int duracion)
-        /*MODIFICACION*/
-        System.out.println("""
-                           ***********************
-                           Elige que quieres hacer
-                           1-Cambiar Autor
-                           2-Cambiar Titulo
-                           3-Cambiar genero
-                           4-Cambiar Duracion
-                           5-Salir""");
-        int aux;
-        String auxS;
-        if (ent.hasNextInt()) {
-            aux = ent.nextInt();
-            switch (aux) {
-                case 1:
-                    // 1-Cambiar Autor
-                    System.out.println("Escribe el nuevo nombre");
-                    auxS = ent.nextLine();
-                    totalDiscos[NUMDISCOS].setGenero(auxS);
-                    break;
-                case 2:
-                    // 2-Cambiar Titulo
-                    Anyadir();
-                    TextoDefault();
-                    break;
-                case 3:
-                    // 3-Cambiar genero
-                    Borrar();
-                    TextoDefault();
-                    break;
-                case 4:
-                    // 4-Cambiar Duracion
-                    Modificar();
-                    TextoDefault();
-                    break;
-                case 5:
-                    // 5-Salir
-                    flag = true;
-                    break;
-                default:
-                    // Volver a pregutnar
-                    System.out.println("Opcion incorrecta vuelve a elegir");
-            }
-        } else {
-            System.out.println("Por favor introduce un numero valido");
-        }
 
     }
 
@@ -260,11 +215,11 @@ public class DiscosMain {
         System.out.println("""
                            ***********************
                            Elige que quieres hacer
-                           1-Listar
-                           2-Anyadir
-                           3-Borrar
-                           4-Modificar
-                           5-Salir""");
+                           [1] -Listar
+                           [2] -Anyadir
+                           [3] -Borrar
+                           [4] -Modificar
+                           [5] -Salir""");
     }
 
 }//FIN
