de037597d10280c8913e96546216248d58ec1e3a
Alumno <Alumno@PC15690>
2023-04-27 09:44:37 +0200

    dzfgzdgd

---
+++ src/main/java/Uf5/E005PanelMain.java
@@ -0,0 +1,12 @@
+
+package Uf5;
+
+
+public class E005PanelMain {
+
+
+    public static void main(String[] args) {
+        E005Panel ventanaPanel = new E005Panel("Ventana con panel increible");
+    }
+    
+}
---
+++ src/main/java/Uf5/E007EtiquetaMain.java
@@ -0,0 +1,12 @@
+
+package Uf5;
+
+
+public class E007EtiquetaMain {
+
+
+    public static void main(String[] args) {
+        E007Etiqueta ventanaPanel = new E007Etiqueta("Ventana con panel increible");
+    }
+    
+}
