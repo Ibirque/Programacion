a5b45e0b7bd71315c80ccb38b864d832ad667a4c
Ibir <Ibir@192.168.0.173>
2023-11-10 06:40:11 +0100

    Servicios y procesos casi completado

--- src/main/java/ServiciosYProcesos/VigenerePardoIbirqueMain.java
+++ src/main/java/ServiciosYProcesos/VigenerePardoIbirqueMain.java
@@ -24,23 +24,18 @@ import java.util.Scanner;
  * usando una entidad certificadora.
  *
  */
-
-
-
-
 public class VigenerePardoIbirqueMain {
-    
-    //Creamos un abecedario global para facil acceso de las funciones
-    private char[] alfabeto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+    //Creamos un abecedario global para facil acceso de las funciones
+    public char[] letras = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public static void main(String[] args) {
-       VigenerePardoIbirqueMain prog = new VigenerePardoIbirqueMain();
-       prog.inicio();
+        VigenerePardoIbirqueMain prog = new VigenerePardoIbirqueMain();
+        prog.inicio();
     }
 
-    public void inicio(){
-    // Quiero: Menu(), FuncionDeEncriptación(), FunciónDesencriptado().
+    public void inicio() {
+        // Quiero: Menu(), FuncionDeEncriptación(), FunciónDesencriptado().
         // Opcional: Entidad certificadora.
 
         // flag
@@ -61,10 +56,37 @@ public class VigenerePardoIbirqueMain {
                 entrada = ent.nextInt();
 
                 switch (entrada) {
-                    case 1 -> //Codificar
-                        FuncionDeEncriptacion();
-                    case 2 -> //Desencriptar
-                        FunciónDesencriptado();
+                    case 1 -> {
+                        //Codificar
+                        Scanner ent2 = new Scanner(System.in);
+
+                        //Recogemos input
+                        System.out.println("Paso 1 - Introduce el texto");
+                        String miTexto;
+                        miTexto = ent2.nextLine().toLowerCase();
+
+                        System.out.println("Paso 2 - Introduce la clave");
+                        String miClave;
+                        miClave = ent2.nextLine().toLowerCase();
+
+                        System.out.println(FuncionDeEncriptacion(miTexto, miClave));
+
+                    }
+                    case 2 -> {
+                        //Desencriptar
+                        Scanner ent3 = new Scanner(System.in);
+
+                        //Recogemos input
+                        System.out.println("Paso 1 - Introduce el texto");
+                        String miTexto;
+                        miTexto = ent3.nextLine().toLowerCase();
+
+                        System.out.println("Paso 2 - Introduce la clave");
+                        String miClave;
+                        miClave = ent3.nextLine().toLowerCase();
+
+                        System.out.println(FuncionDesencriptado(miTexto, miClave));
+                    }
                     case 3 -> { //Salir
                         System.out.println("Que tengas un buen dia");
                         bandera = true;
@@ -93,25 +115,90 @@ public class VigenerePardoIbirqueMain {
                            """);
     }
 
-    //Entra mi texto y mi clave, sale el texto cifrado
+    //Entra mi texto y mi clave, sale el texto entero cifrado
     String FuncionDeEncriptacion(String miTexto, String miClave) {
-        String a = "test";
+        /*String a = "";
         char letra;
         char claveBuffer;
-        
+
         //Bucle para recorrer mi abecedario
-        for(int i = 0; i<miTexto.length(); i++){
-            letra = miTexto.charAt(i);
-            //usamos "%" para el caracter correcto, la clave peude ser mas pequeña que el texto original
-            claveBuffer = miClave.charAt(i%miClave.length());
+        for (int i = 0; i < miTexto.length(); i++) {
+            letra = miTexto.charAt(i);   
             
+            //usamos "%" para el caracter correcto, la clave peude ser mas pequeña que el texto original
+            claveBuffer = miClave.charAt(i % miClave.length());
+
+            a += letras[cifrado(letra, claveBuffer)];
         }
-        
+
         return a;
+        */
+
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < miTexto.length(); i++) {
+            char letra = miTexto.charAt(i);
+
+            // Verificar si la letra es un carácter alfabético
+            if (Character.isLetter(letra)) {
+                char claveBuffer = miClave.charAt(i % miClave.length());
+                int posicionCifrada = cifrado(letra, claveBuffer);
+
+                // Verificar si la posición cifrada es un índice válido
+                if (posicionCifrada != -1) {
+                    resultado.append(letras[posicionCifrada]);
+                } else {
+                    // Puedes manejar el carácter no alfabético aquí (omitirlo, mostrar un mensaje, etc.)
+                }
+            } else {
+                // Mantener caracteres no alfabéticos sin cambios
+                resultado.append(letra);
+            }
+        }
+
+        return resultado.toString();
+
     }
 
-    public static void FunciónDesencriptado() {
+    //Entran el texto y la clave, sale solo un caracter cifrado, llamamos varias veces a esta funcion desde el bucle
+    public int cifrado(char miTexto, char miClave) {
+        int pos;
+        int posClave;
 
+        if (Character.isLetter(miClave)) {
+            pos = new String(letras).indexOf(miTexto);
+            posClave = new String(letras).indexOf(miClave);
+
+            return (pos + posClave) % letras.length;
+        } else {
+            return -1;
+        }
+    }
+
+    //Entra el texto cifrado y la clave, sale texto limpio
+    //Es un clon de la encriptacion
+    public String FuncionDesencriptado(String miTexto, String miClave) {
+        String a = "";
+        char letra;
+        char claveBuffer;
+        char letraDescifrada;
+        int pos = 0;
+
+        //Bucle para recorrer mi abecedario
+        for (int i = 0; i < miTexto.length(); i++) {
+            letra = miTexto.charAt(i);
+            //usamos "%" para el caracter correcto, la clave peude ser mas pequeña que el texto original
+            claveBuffer = miClave.charAt(i % miClave.length());
+
+            //Ciframos la letra
+            letraDescifrada = (char) ((letra - claveBuffer + 26) % 26 + 'a');
+
+            a += letraDescifrada;
+
+            pos = letra;
+        }
+
+        return a;
     }
 
 }
